38077c8ca61d0fbcac95746e98d17fdb024575a7
castroDAMC <47617047+castroDAMC@users.noreply.github.com>
2022-04-19 21:37:56 -0400

    Update Constants.kt

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
 
-    const val API_KEY = "HpVHKF9LLtZJlec1ExEXFQFe2pLVyXN0dlXjXVKA"
+    const val API_KEY = "INSERT API KEY HERE"
 
     const val PICTURE_DONE = 2
     const val PICTURE_LOADING = 1
